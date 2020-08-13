02f28dce946ba0b46f2f42b0cd3129fb4054324c
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2020-08-13 19:18:22 +0800

    admin table changes

--- my_store/admin.py
+++ my_store/admin.py
@@ -12,7 +12,7 @@ def make_avail(modeladmin, request, queryset):
 make_avail.short_description = "Select Available Products"
 
 class The_Prod_Cat(admin.ModelAdmin):
-    list_display = ('title', 'Product_Catgeory', 'Price', 'available')
+    list_display = ('title', 'Product_Catgeory', 'Price', 'discount', 'item_thresh', 'available')
     search_fields = ('title',)
     list_filter = ('product_category',)
     actions = (make_avail,)
